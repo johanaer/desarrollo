30f1040778aef60f1f7ab823a0afb4598a46f58d
johana <johanarodriguez310@gmail.com>
2024-08-19 01:49:41 -0600

    eliminacion de archivo js

--- javascript.js
+++
@@ -1,6 +0,0 @@
-function addToCart(pizzaName) {
-    const cartItems = document.getElementById('cart-items');
-    const newItem = document.createElement('li');
-    newItem.textContent = pizzaName;
-    cartItems.appendChild(newItem);
-}
